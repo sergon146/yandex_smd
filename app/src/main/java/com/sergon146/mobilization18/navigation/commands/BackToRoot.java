372f927a31d8eddd9973c8b60a1eb7a94491447f
sergon146 <sergon146@gmail.com>
2018-07-26 14:33:06 +0300

    HW-2.1 tab bar navigation

---
+++ app/src/main/java/com/sergon146/mobilization18/navigation/commands/BackToRoot.java
@@ -0,0 +1,10 @@
+package com.sergon146.mobilization18.navigation.commands;
+
+import ru.terrakok.cicerone.commands.Command;
+
+/**
+ * @author Andrey Zolotov (andrey.zolotov@simbirsoft.com
+ * @since 09.06.2018.
+ */
+public class BackToRoot implements Command {
+}
---
+++ app/src/main/java/com/sergon146/mobilization18/ui/main/TabBarScreens.java
@@ -0,0 +1,26 @@
+package com.sergon146.mobilization18.ui.main;
+
+import com.sergon146.mobilization18.R;
+
+public enum TabBarScreens {
+    MAIN(R.string.tab_main, R.drawable.ic_main_inactive),
+    FEED(R.string.tab_feed, R.drawable.ic_bag_inactive),
+    REPORT(R.string.tab_report, R.drawable.ic_report_inactive),
+    PROFILE(R.string.tab_profile, R.drawable.ic_user_inactive);
+
+    private int title;
+    private int icon;
+
+    TabBarScreens(int title, int icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+}
